0d486b717c5935d96e4dba923cbaebb5ee73fa4f
Diego de Biagi <diegobiagiviana@gmail.com>
2016-10-13 21:43:56 -0300

    The bundle is now functional on sf3.1. But still need some fixes.

--- Form/LogSearchType.php
+++ Form/LogSearchType.php
@@ -8,39 +8,41 @@ use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\OptionsResolver\Options;
-
+use Symfony\Component\Form\Extension\Core\Type as FormTypes;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\DBAL\Connection;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * @author Jeremy Barthe <j.barthe@lexik.fr>
  */
-class LogSearchType extends AbstractType
-{
+class LogSearchType extends AbstractType {
+
+    const NAME = 'search';
+
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('term', 'search', array(
+            ->add('term', FormTypes\SearchType::class, array(
                 'required' => false,
             ))
-            ->add('level', 'choice', array(
-                'choices'     => $options['log_levels'],
-                'required'    => false,
+            ->add('level', FormTypes\ChoiceType::class, array(
+                'choices' => $options['log_levels'],
+                'required' => false,
             ))
-            ->add('date_from', 'datetime', array(
+            ->add('date_from', FormTypes\DateTimeType::class, array(
                 'date_widget' => 'single_text',
                 'date_format' => 'MM/dd/yyyy',
                 'time_widget' => 'text',
-                'required'    => false,
+                'required' => false,
             ))
-            ->add('date_to', 'datetime', array(
+            ->add('date_to', FormTypes\DateTimeType::class, array(
                 'date_widget' => 'single_text',
                 'date_format' => 'MM/dd/yyyy',
                 'time_widget' => 'text',
-                'required'    => false,
+                'required' => false,
             ))
         ;
 
@@ -49,48 +51,62 @@ class LogSearchType extends AbstractType
             return Type::getType('datetime')->convertToDatabaseValue($date, $qb->getConnection()->getDatabasePlatform());
         };
 
-        $builder->addEventListener(FormEvents::POST_BIND, function(FormEvent $event) use ($qb, $convertDateToDatabaseValue) {
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) use ($qb, $convertDateToDatabaseValue) {
             $data = $event->getData();
 
             if (null !== $data['term']) {
                 $qb->andWhere('l.message LIKE :message')
-                   ->setParameter('message', '%'.str_replace(' ', '%', $data['term']).'%')
-                   ->orWhere('l.channel LIKE :channel')
-                   ->setParameter('channel', $data['term'].'%');
+                    ->setParameter('message', '%' . str_replace(' ', '%', $data['term']) . '%')
+                    ->orWhere('l.channel LIKE :channel')
+                    ->setParameter('channel', $data['term'] . '%');
             }
 
             if (null !== $data['level']) {
                 $qb->andWhere('l.level = :level')
-                   ->setParameter('level', $data['level']);
+                    ->setParameter('level', $data['level']);
             }
 
             if ($data['date_from'] instanceof \DateTime) {
                 $qb->andWhere('l.datetime >= :date_from')
-                   ->setParameter('date_from', $convertDateToDatabaseValue($data['date_from']));
+                    ->setParameter('date_from', $convertDateToDatabaseValue($data['date_from']));
             }
 
             if ($data['date_to'] instanceof \DateTime) {
                 $qb->andWhere('l.datetime <= :date_to')
-                   ->setParameter('date_to', $convertDateToDatabaseValue($data['date_to']));
+                    ->setParameter('date_to', $convertDateToDatabaseValue($data['date_to']));
             }
         });
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver
             ->setRequired(array(
                 'query_builder',
             ))
             ->setDefaults(array(
-                'log_levels'      => array(),
+                'log_levels' => array(),
                 'csrf_protection' => false,
             ))
-            ->setAllowedTypes(array(
-                'log_levels'    => 'array',
+            ->setAllowedTypes('log_levels', 'array')
+            ->setAllowedTypes('query_builder', '\Doctrine\DBAL\Query\QueryBuilder')
+            
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver
+            ->setRequired(array(
+                'query_builder',
+            ))
+            ->setDefaults(array(
+                'log_levels' => array(),
+                'csrf_protection' => false,
+            ))
+            ->setAllowedTypes('search', array(
+                'log_levels' => 'array',
                 'query_builder' => '\Doctrine\DBAL\Query\QueryBuilder',
             ))
         ;
@@ -99,8 +115,12 @@ class LogSearchType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getName()
-    {
-        return 'search';
+    public function getName() {
+        return static::NAME;
     }
+
+    public function getBlockPrefix() {
+        return static::NAME;
+    }
+
 }
