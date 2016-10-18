3bb9ea6f28d8cb72a9a93ac5cf0505f50ff504c2
Diego de Biagi <diegobiagiviana@gmail.com>
2016-10-18 00:43:13 -0200

    Finally fixed date filter issue. Upgraded to bootstrap 3. Changed datepicker to the bootstrap datepicker. Changed filter form layout to horizontal layout.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,36 +4,31 @@ namespace Lexik\Bundle\MonologBrowserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
 use Doctrine\DBAL\DBALException;
-
 use Lexik\Bundle\MonologBrowserBundle\Form\LogSearchType;
 
 /**
  * @author Jeremy Barthe <j.barthe@lexik.fr>
  */
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         try {
             $query = $this->getLogRepository()->getLogsQueryBuilder();
 
             $filter = $this->createForm(LogSearchType::class, null, array(
                 'query_builder' => $query,
-                'log_levels'    => $this->getLogRepository()->getLogsLevel(),
+                'log_levels' => $this->getLogRepository()->getLogsLevel(),
             ));
-
+            
             $filter->submit($request->get($filter->getName()));
-
+            
             $pagination = $this->get('knp_paginator')->paginate(
-                $query,
-                $request->query->get('page', 1),
-                $this->container->getParameter('lexik_monolog_browser.logs_per_page')
+                $query, $request->query->get('page', 1), $this->container->getParameter('lexik_monolog_browser.logs_per_page')
             );
         } catch (DBALException $e) {
             $this->get('session')->getFlashBag()->add('error', $e->getMessage());
@@ -41,9 +36,9 @@ class DefaultController extends Controller
         }
 
         return $this->render('LexikMonologBrowserBundle:Default:index.html.twig', array(
-            'filter'      => isset($filter) ? $filter->createView() : null,
-            'pagination'  => $pagination,
-            'base_layout' => $this->getBaseLayout(),
+                'filter' => isset($filter) ? $filter->createView() : null,
+                'pagination' => $pagination,
+                'base_layout' => $this->getBaseLayout(),
         ));
     }
 
@@ -53,8 +48,7 @@ class DefaultController extends Controller
      * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      */
-    public function showAction(Request $request, $id)
-    {
+    public function showAction(Request $request, $id) {
         $log = $this->getLogRepository()->getLogById($id);
 
         if (null === $log) {
@@ -64,31 +58,28 @@ class DefaultController extends Controller
         $similarLogsQuery = $this->getLogRepository()->getSimilarLogsQueryBuilder($log);
 
         $similarLogs = $this->get('knp_paginator')->paginate(
-            $similarLogsQuery,
-            $request->query->get('page', 1),
-            10
+            $similarLogsQuery, $request->query->get('page', 1), 10
         );
 
         return $this->render('LexikMonologBrowserBundle:Default:show.html.twig', array(
-            'log'          => $log,
-            'similar_logs' => $similarLogs,
-            'base_layout'  => $this->getBaseLayout(),
+                'log' => $log,
+                'similar_logs' => $similarLogs,
+                'base_layout' => $this->getBaseLayout(),
         ));
     }
 
     /**
      * @return string
      */
-    protected function getBaseLayout()
-    {
+    protected function getBaseLayout() {
         return $this->container->getParameter('lexik_monolog_browser.base_layout');
     }
 
     /**
      * @return \Lexik\Bundle\MonologBrowserBundle\Model\LogRepository
      */
-    protected function getLogRepository()
-    {
+    protected function getLogRepository() {
         return $this->get('lexik_monolog_browser.model.log_repository');
     }
+
 }
--- Form/LogSearchType.php
+++ Form/LogSearchType.php
@@ -7,10 +7,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\Form\Extension\Core\Type as FormTypes;
 use Doctrine\DBAL\Types\Type;
-use Doctrine\DBAL\Connection;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -26,26 +24,35 @@ class LogSearchType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('term', FormTypes\SearchType::class, array(
-                'required' => false,
+                'required' => false
             ))
             ->add('level', FormTypes\ChoiceType::class, array(
-                'placeholder' => 'log.search.all',
                 'translation_domain' => 'LexikMonologBrowserBundle',
                 'choices' => $options['log_levels'],
                 'required' => false,
             ))
-            ->add('date_from', FormTypes\DateTimeType::class, array(
-                'date_widget' => 'single_text',
-                'date_format' => 'MM/dd/yyyy',
-                'time_widget' => 'text',
+            ->add('date_from', FormTypes\DateType::class, array(
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'html5' => false,
+                'required' => false
+            ))
+            ->add('time_from', FormTypes\TimeType::class, [
+                'widget' => 'single_text',
+                'input'  => 'timestamp',
+                'required' => false
+            ])
+            ->add('date_to', FormTypes\DateType::class, array(
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'html5' => false,
                 'required' => false,
             ))
-            ->add('date_to', FormTypes\DateTimeType::class, array(
-                'date_widget' => 'single_text',
-                'date_format' => 'MM/dd/yyyy',
-                'time_widget' => 'text',
-                'required' => false,
-            ))
+            ->add('time_to', FormTypes\TimeType::class, [
+                'widget' => 'single_text',
+                'input'  => 'timestamp',
+                'required' => false
+            ])
         ;
 
         $qb = $options['query_builder'];
@@ -55,7 +62,7 @@ class LogSearchType extends AbstractType {
 
         $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) use ($qb, $convertDateToDatabaseValue) {
             $data = $event->getData();
-
+            
             if (null !== $data['term']) {
                 $qb->andWhere('l.message LIKE :message')
                     ->setParameter('message', '%' . str_replace(' ', '%', $data['term']) . '%')
@@ -67,13 +74,19 @@ class LogSearchType extends AbstractType {
                 $qb->andWhere('l.level = :level')
                     ->setParameter('level', $data['level']);
             }
-
+            
             if ($data['date_from'] instanceof \DateTime) {
+                if(isset($data['time_from']) && $data['time_from']){
+                    $data['date_from']->setTimestamp($data['date_from']->getTimestamp() + $data['time_from']);
+                }
                 $qb->andWhere('l.datetime >= :date_from')
                     ->setParameter('date_from', $convertDateToDatabaseValue($data['date_from']));
             }
 
             if ($data['date_to'] instanceof \DateTime) {
+                if(isset($data['time_to']) && $data['time_to']){
+                    $data['date_to']->setTimestamp($data['date_to']->getTimestamp() + $data['time_to']);
+                }
                 $qb->andWhere('l.datetime <= :date_to')
                     ->setParameter('date_to', $convertDateToDatabaseValue($data['date_to']));
             }
@@ -91,7 +104,7 @@ class LogSearchType extends AbstractType {
             ))
             ->setAllowedTypes('log_levels', 'array')
             ->setAllowedTypes('query_builder', '\Doctrine\DBAL\Query\QueryBuilder')
-            
+
         ;
     }
 
